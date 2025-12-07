21d6218de4f7ac5c51ae60708e7a34b4fb6a9bfb
Logesh <loguwork1157@gmail.com>
2025-12-07 11:40:30 +0530

    modelform with html update

--- inventory_demo/inventory/forms.py
+++ inventory_demo/inventory/forms.py
@@ -5,3 +5,10 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'quantity', 'price', 'description']
+
+# class ProductForm(forms.Form):
+#     name = forms.CharField(max_length=150)
+#     quantity = forms.IntegerField(min_value=0)
+#     price = forms.DecimalField(max_digits=8, decimal_places=2)
+#     description = forms.CharField(widget=forms.Textarea, required=False)
+#     expiry = forms.DateField(required=False, widget=forms.SelectDateWidget)
--- inventory_demo/inventory/views.py
+++ inventory_demo/inventory/views.py
@@ -13,18 +13,10 @@ def product_list(request):
 # Add new product using manual HTML form
 def product_create(request):
     if request.method == 'POST':
+        post_form = ProductForm(request.POST)
+        if post_form.is_valid():
+            post_form.save()
 
-        user_name = request.POST.get('name')
-        quantity = request.POST.get('quantity')
-        user_price = request.POST.get('price')
-        description = request.POST.get('description')
-
-        Product.objects.create(
-            name=user_name,
-            quantity=quantity,
-            price=user_price,
-            description=description
-        )
         return redirect('product_list')
     else:
         form = ProductForm()
@@ -42,7 +34,9 @@ def product_update(request, pk):
         product.save()
         return redirect('product_list')
 
-    return render(request, 'inventory/product_form.html', {'product': product})
+    if product:
+        form = ProductForm(instance=product)
+    return render(request, 'inventory/product_form.html', {'form': form})
 
 
 # Delete a product
